1938a9eeedd94884c83fca3ed0834ede74e08033
george-tr6 <george.tr6@gmail.com>
2018-10-05 18:20:35 -0700

    base app structure done

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto doloribus amet at? Deserunt, neque dolor.</p>
+        
+    </div>
+)
